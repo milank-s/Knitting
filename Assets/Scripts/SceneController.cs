b5aca55801e2c447dae0e54ad70c5dd198d4ffe3
Milan Koerner-Safrata <mks554@nyu.edu>
2021-06-10 23:57:12 -0400

    level tweaks. Dumb error with level incrementing

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -165,7 +165,8 @@ public class SceneController : MonoBehaviour
             if (curLevelSet.isScene)
             {
                 Services.main.LoadScene();
-                curLevel++;
+                //what why
+                //curLevel++;
             }
             else
             {
